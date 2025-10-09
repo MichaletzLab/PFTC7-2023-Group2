6a3b2da379449554ed83495a0933dfa78e7745c1
Marcella Cross <mjc467@student.ubc.ca>
2025-10-09 11:55:39 -0700

    ignorestuff

--- Analyses 9.5.25/Models.R
+++
@@ -1,9 +0,0 @@
-# ---- Fit GAMs with just Tleaf + PC1 + Species ----
-gam_mod_A_PC1   <- gam(A    ~ s(Tleaf, k=3) + s(PC1, k=3) + ti(Tleaf, PC1, k=3) + Species,
-                       data = raw.env.data_pca, method = "REML")
-gam_mod_E_PC1   <- gam(E    ~ s(Tleaf, k=3) + s(PC1, k=3) + ti(Tleaf, PC1, k=3) + Species,
-                       data = raw.env.data_pca, method = "REML")
-gam_mod_gsw_PC1 <- gam(gsw  ~ s(Tleaf, k=3) + s(PC1, k=3) + ti(Tleaf, PC1, k=3) + Species,
-                       data = raw.env.data_pca, method = "REML")
-gam_mod_iWUE_PC1<- gam(iWUE ~ s(Tleaf, k=3) + s(PC1, k=3) + ti(Tleaf, PC1, k=3) + Species,
-                       data = raw.env.data_pca, method = "REML")
