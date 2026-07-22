423417db0055d835420a5e831ab7ab3258f04592
Sean Michaletz <seanmichaletz@users.noreply.github.com>
2026-07-22 09:52:05 +0200

    Added site factor-smooth to Fig 3 GAMs to correct PC1 pseudoreplication

--- Analysis Code/Models.R
+++ Analysis Code/Models.R
@@ -1,6 +1,39 @@
 # libraries
 library(mgcv)
 
+# For Fig 3 GAMs, PC1 is a site-level predictor, it takes one value per site
+# (9 total), so curves and points within a site aren't independent. Without a 
+# site-level term, the tests on s(PC1) and ti(Tleaf, PC1) treat within-site 
+# observatins as independent (pseudoreplication). s(Tleaf, Site, bs="fs")
+# gives each site its own temperature response, taking care of site-level 
+# variation so s(PC1) and ti(Tleaf, PC1) are tested against real between-site
+# variation. This is similar to how site random effects added in Fig. 4 and 5
+# mixed models.
+
+# Site corresponds to unique elevations, which is grouping that PC1 varies at
+# (for the factor smooth)
+raw.env.data_pca$Site <- factor(raw.env.data_pca$Elevation)
+
+gam_mod_A_PC1 <- gam(A ~ s(Tleaf, k=5) + s(PC1, k=5) + ti(Tleaf, PC1, k=5)
+                     + Species + s(curveID, bs="re") + s(Tleaf, Site, bs="fs", k=5),
+                     data = raw.env.data_pca, method = "REML")
+
+gam_mod_E_PC1 <- gam(E ~ s(Tleaf, k=5) + s(PC1, k=5) + ti(Tleaf, PC1, k=5)
+                     + Species + s(curveID, bs="re") + s(Tleaf, Site, bs="fs", k=5),
+                     data = raw.env.data_pca, method = "REML")
+
+gam_mod_gsw_PC1 <- gam(gsw ~ s(Tleaf, k=5) + s(PC1, k=5) + ti(Tleaf, PC1, k=5)
+                       + Species + s(curveID, bs="re") + s(Tleaf, Site, bs="fs", k=5),
+                       data = raw.env.data_pca, method = "REML")
+
+gam_mod_iWUE_PC1 <- gam(iWUE ~ s(Tleaf, k=5) + s(PC1, k=5) + ti(Tleaf, PC1, k=5)
+                        + Species + s(curveID, bs="re") + s(Tleaf, Site, bs="fs", k=5),
+                        data = raw.env.data_pca, method = "REML")
+
+gam_mod_WUE_PC1 <- gam(WUE ~ s(Tleaf, k=5) + s(PC1, k=5) + ti(Tleaf, PC1, k=5)
+                       + Species + s(curveID, bs="re") + s(Tleaf, Site, bs="fs", k=5),
+                       data = raw.env.data_pca, method = "REML")
+
 # ---- Fit GAMs ----
 gam_mod_A_PC1   <- gam(A    ~ s(Tleaf, k=5) + s(PC1, k=5) + ti(Tleaf, PC1, k=5) 
                        + Species + s(curveID, bs="re"),                       
