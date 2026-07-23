00dd4c448b27bcd2b772448fb52f08fce2fc138b
Sean Michaletz <seanmichaletz@users.noreply.github.com>
2026-07-23 12:19:00 +0200

    Add PC1 only models for Fig. S3, and coordinate s(Tleaf, k=5) in SI code

--- Analysis Code/Models.R
+++ Analysis Code/Models.R
@@ -42,7 +42,7 @@ summary(gam_mod_WUE_PC1)
 
 
 #For supplemental
-gam_mod_full_environ_A <- gam(A ~ s(Tleaf, k=4) + 
+gam_mod_full_environ_A <- gam(A ~ s(Tleaf, k=5) + 
                                s(mean_moist_pct, k=4) + 
                                s(mean_T1, k=4) + 
                                s(mean_T2, k=4) + 
@@ -51,7 +51,7 @@ gam_mod_full_environ_A <- gam(A ~ s(Tleaf, k=4) +
                         Species+ s(curveID, bs="re"),                       
                       data = raw.env.data_pca, method = "REML")
 summary(gam_mod_full_environ_A)
-gam_mod_full_environ_E <- gam(E ~ s(Tleaf, k=4) + 
+gam_mod_full_environ_E <- gam(E ~ s(Tleaf, k=5) + 
                                s(mean_moist_pct, k=4) + 
                                s(mean_T1, k=4) + 
                                s(mean_T2, k=4) + 
@@ -59,7 +59,7 @@ gam_mod_full_environ_E <- gam(E ~ s(Tleaf, k=4) +
                                s(vegetation_height, k=4) + 
                                Species+ s(curveID, bs="re"),                       
                              data = raw.env.data_pca, method = "REML")
-gam_mod_full_environ_gsw <- gam(gsw ~ s(Tleaf, k=4) + 
+gam_mod_full_environ_gsw <- gam(gsw ~ s(Tleaf, k=5) + 
                                s(mean_moist_pct, k=4) +
                                s(mean_T1, k=4) +
                                s(mean_T2, k=4) + 
@@ -67,7 +67,7 @@ gam_mod_full_environ_gsw <- gam(gsw ~ s(Tleaf, k=4) +
                                s(vegetation_height, k=4) + 
                                Species+ s(curveID, bs="re"),                       
                              data = raw.env.data_pca, method = "REML")
-gam_mod_full_environ_iWUE <- gam(iWUE ~ s(Tleaf, k=4) + 
+gam_mod_full_environ_iWUE <- gam(iWUE ~ s(Tleaf, k=5) + 
                                  s(mean_moist_pct, k=4) +
                                  s(mean_T1, k=4) + 
                                  s(mean_T2, k=4) + 
@@ -75,7 +75,7 @@ gam_mod_full_environ_iWUE <- gam(iWUE ~ s(Tleaf, k=4) +
                                  s(vegetation_height, k=4) + 
                                  Species+ s(curveID, bs="re"),                       
                                data = raw.env.data_pca, method = "REML")
-gam_mod_full_environ_WUE <- gam(WUE ~ s(Tleaf, k=4) + 
+gam_mod_full_environ_WUE <- gam(WUE ~ s(Tleaf, k=5) + 
                                    s(mean_moist_pct, k=4) + 
                                    s(mean_T1, k=4) + 
                                    s(mean_T2, k=4) + 
@@ -83,7 +83,7 @@ gam_mod_full_environ_WUE <- gam(WUE ~ s(Tleaf, k=4) +
                                    s(vegetation_height, k=4) + 
                                    Species+ s(curveID, bs="re"),                       
                                  data = raw.env.data_pca, method = "REML")
-gam_mod_full_PC_A <- gam(A ~ s(Tleaf, k=4) + 
+gam_mod_full_PC_A <- gam(A ~ s(Tleaf, k=5) + 
                                    s(PC1, k=4) +
                                    s(PC2, k=4) +
                                    s(PC3, k=4) + 
@@ -91,7 +91,7 @@ gam_mod_full_PC_A <- gam(A ~ s(Tleaf, k=4) +
                                    s(PC5, k=4) +  
                                    Species+ s(curveID, bs="re"),                       
                                  data = raw.env.data_pca, method = "REML")
-gam_mod_full_PC_E <- gam(E ~ s(Tleaf, k=4) + 
+gam_mod_full_PC_E <- gam(E ~ s(Tleaf, k=5) + 
                            s(PC1, k=4) + 
                            s(PC2, k=4) + 
                            s(PC3, k=4) + 
@@ -99,7 +99,7 @@ gam_mod_full_PC_E <- gam(E ~ s(Tleaf, k=4) +
                            s(PC5, k=4) + 
                            Species+ s(curveID, bs="re"),                       
                          data = raw.env.data_pca, method = "REML")
-gam_mod_full_PC_gsw <- gam(gsw ~ s(Tleaf, k=4) + 
+gam_mod_full_PC_gsw <- gam(gsw ~ s(Tleaf, k=5) + 
                            s(PC1, k=4) + 
                            s(PC2, k=4) + 
                            s(PC3, k=4) +
@@ -107,7 +107,7 @@ gam_mod_full_PC_gsw <- gam(gsw ~ s(Tleaf, k=4) +
                            s(PC5, k=4) +  
                            Species+ s(curveID, bs="re"),                       
                          data = raw.env.data_pca, method = "REML")
-gam_mod_full_PC_iWUE <- gam(iWUE ~ s(Tleaf, k=4) + 
+gam_mod_full_PC_iWUE <- gam(iWUE ~ s(Tleaf, k=5) + 
                            s(PC1, k=4) + 
                            s(PC2, k=4) +
                            s(PC3, k=4) +
@@ -115,7 +115,7 @@ gam_mod_full_PC_iWUE <- gam(iWUE ~ s(Tleaf, k=4) +
                            s(PC5, k=4) +  
                            Species+ s(curveID, bs="re"),                       
                          data = raw.env.data_pca, method = "REML")
-gam_mod_full_PC_WUE <- gam(WUE ~ s(Tleaf, k=4) + 
+gam_mod_full_PC_WUE <- gam(WUE ~ s(Tleaf, k=5) + 
                               s(PC1, k=4) + 
                               s(PC2, k=4) +
                               s(PC3, k=4) +  
@@ -124,5 +124,39 @@ gam_mod_full_PC_WUE <- gam(WUE ~ s(Tleaf, k=4) +
                               Species+ s(curveID, bs="re"),                       
                             data = raw.env.data_pca, method = "REML")
 
+# Figure S3, third arm (PC1 only)
+# All three S3 arms leave out s(Tleaf, Site, bs="fs") on purpose. S3 asks
+# whether PC1 on its own captures the environmental signal as well as the full
+# set of variables or all five PCs. All those predictors are site-level,
+# one value per site, so if we also give each site its own free temperature
+# response, that term takes up whatever the environmental terms miss. All three
+# arms would then agree no matter what, even if PC1 were a poor summary, and the
+# figure wouldn't test anything.
+#
+# These are diagnostic models (only used for the S3 curves) - not used for
+# inference in the paper, and the Fig. 3 models (above) keep the site term.
+#
+# Named gam_mod_PC1_only_* rather than gam_mod_*_PC1 so they cannot collide with
+# the main text objects.
+
+gam_mod_PC1_only_A <- gam(A ~ s(Tleaf, k=5) + s(PC1, k=5) + ti(Tleaf, PC1, k=5)
+                          + Species + s(curveID, bs="re"),
+                          data = raw.env.data_pca, method = "REML")
+
+gam_mod_PC1_only_E <- gam(E ~ s(Tleaf, k=5) + s(PC1, k=5) + ti(Tleaf, PC1, k=5)
+                          + Species + s(curveID, bs="re"),
+                          data = raw.env.data_pca, method = "REML")
+
+gam_mod_PC1_only_gsw <- gam(gsw ~ s(Tleaf, k=5) + s(PC1, k=5) + ti(Tleaf, PC1, k=5)
+                            + Species + s(curveID, bs="re"),
+                            data = raw.env.data_pca, method = "REML")
+
+gam_mod_PC1_only_iWUE <- gam(iWUE ~ s(Tleaf, k=5) + s(PC1, k=5) + ti(Tleaf, PC1, k=5)
+                             + Species + s(curveID, bs="re"),
+                             data = raw.env.data_pca, method = "REML")
+
+gam_mod_PC1_only_WUE <- gam(WUE ~ s(Tleaf, k=5) + s(PC1, k=5) + ti(Tleaf, PC1, k=5)
+                            + Species + s(curveID, bs="re"),
+                            data = raw.env.data_pca, method = "REML")
 
 
