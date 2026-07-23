e6d6343c304302cdd7357343a09c9d93d229eba2
Sean Michaletz <seanmichaletz@users.noreply.github.com>
2026-07-23 09:42:06 +0200

    Removed duplicate GAM block

--- Analysis Code/Models.R
+++ Analysis Code/Models.R
@@ -34,33 +34,6 @@ gam_mod_WUE_PC1 <- gam(WUE ~ s(Tleaf, k=5) + s(PC1, k=5) + ti(Tleaf, PC1, k=5)
                        + Species + s(curveID, bs="re") + s(Tleaf, Site, bs="fs", k=5),
                        data = raw.env.data_pca, method = "REML")
 
-# ---- Fit GAMs ----
-gam_mod_A_PC1   <- gam(A    ~ s(Tleaf, k=5) + s(PC1, k=5) + ti(Tleaf, PC1, k=5) 
-                       + Species + s(curveID, bs="re"),                       
-                       data = raw.env.data_pca, method = "REML")
-summary(gam_mod_A_PC1)
-#gam.check(gam_mod_A_PC1)
-
-gam_mod_E_PC1   <- gam(E    ~ s(Tleaf, k=5) + s(PC1, k=5) + ti(Tleaf, PC1, k=5) + 
-                         Species+ s(curveID, bs="re"),                       
-                       data = raw.env.data_pca, method = "REML")
-#gam.check(gam_mod_E_PC1)
-
-gam_mod_gsw_PC1 <- gam(gsw  ~ s(Tleaf, k=5) + s(PC1, k=5) + ti(Tleaf, PC1, k=5) + 
-                         Species + s(curveID, bs="re"),                       
-                       data = raw.env.data_pca, method = "REML")
-#gam.check(gam_mod_gsw_PC1)
-
-gam_mod_iWUE_PC1<- gam(iWUE ~ s(Tleaf, k=5) + s(PC1, k=5) + ti(Tleaf, PC1, k=5) + 
-                         Species+ s(curveID, bs="re"),                       
-                       data = raw.env.data_pca, method = "REML")
-#gam.check(gam_mod_iWUE_PC1)
-
-gam_mod_WUE_PC1<- gam(WUE ~ s(Tleaf, k=5) + s(PC1, k=5) + ti(Tleaf, PC1, k=5) + 
-                         Species+ s(curveID, bs="re"),                       
-                       data = raw.env.data_pca, method = "REML")
-#gam.check(gam_mod_WUE_PC1)
-
 summary(gam_mod_A_PC1)
 summary(gam_mod_E_PC1)
 summary(gam_mod_gsw_PC1)
